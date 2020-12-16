5bf16161722917b7e938ed539c99e137f180ad59
GouadriaHanen <hgouadria@exoplatform.com>
2020-12-16 17:40:31 +0100

    updates

---
+++ services/src/main/java/org/exoplatform/addons/services/Hanen_Service.java
@@ -0,0 +1,14 @@
+package org.exoplatform.addons.services;
+
+import org.exoplatform.services.log.*; 
+
+public class Hanen_Service {
+	 private static final Log LOG = ExoLogger.getExoLogger(Hanen_Service.class);
+	public Hanen_Service() {
+     LOG.info("loged with Hanen_Service! ");
+	}
+	 public void call() {
+		    LOG.info("Hanen_Service call function ");
+		  }
+		
+}
---
+++ services/src/main/java/org/exoplatform/addons/services/Hanen_StartableService.java
@@ -0,0 +1,26 @@
+package org.exoplatform.addons.services;
+
+import org.exoplatform.container.ExoContainerContext;
+import org.exoplatform.container.PortalContainer;
+import org.exoplatform.services.log.*;
+import org.picocontainer.Startable;
+
+public class Hanen_StartableService implements Startable{
+	 private static final Log LOG = ExoLogger.getExoLogger(Hanen_StartableService.class);
+	
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		 LOG.info("loging in from Hanen_Startable Service !");
+		 PortalContainer portalContainer = (PortalContainer) ExoContainerContext.getCurrentContainer();
+		Hanen_Service myNewService = (Hanen_Service) portalContainer.getComponentInstance(Hanen_Service.class);
+		    LOG.info(" Calling function call of Hanen_Service !");
+		    myNewService.call();
+	}
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		 LOG.info("loging out from Hanen_Startable Service !");
+		
+	}
+}
