e0bce4cc2ffbe8115ee615b31af7feefd1971102
GouadriaHanen <hgouadria@exoplatform.com>
2020-12-22 17:53:19 +0100

    JPA Enty with dao

---
+++ services/src/main/java/org/exoplatform/addons/entity/FavoriteActivityEntity.java
@@ -0,0 +1,63 @@
+package org.exoplatform.addons.entity;
+
+import org.exoplatform.commons.api.persistence.ExoEntity;
+import org.exoplatform.social.core.jpa.storage.entity.ActivityEntity;
+import org.exoplatform.social.core.jpa.storage.entity.IdentityEntity;
+
+import javax.persistence.*;
+import java.util.Calendar;
+@ExoEntity
+@Entity
+@Table(name = "ADDON_FAVORITE_ACTIVITY")
+public class FavoriteActivityEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long ID;
+    @Column(name = "Activity_Title")
+    private String ActivityTitle;
+    @Column(name = "Target_Activity")
+    private ActivityEntity TargetActivity;
+    @Column(name = "Favorite_Date")
+    private Calendar favoriteDate;
+
+    public FavoriteActivityEntity() {}
+
+    public FavoriteActivityEntity(Long ID, String activityTitle, ActivityEntity targetActivity, Calendar favoriteDate) {
+        this.ID = ID;
+        ActivityTitle = activityTitle;
+        TargetActivity = targetActivity;
+        this.favoriteDate = favoriteDate;
+    }
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getActivityTitle() {
+        return ActivityTitle;
+    }
+
+    public void setActivityTitle(String activityTitle) {
+        ActivityTitle = activityTitle;
+    }
+
+    public ActivityEntity getTargetActivity() {
+        return TargetActivity;
+    }
+
+    public void setTargetActivity(ActivityEntity targetActivity) {
+        TargetActivity = targetActivity;
+    }
+
+    public Calendar getFavoriteDate() {
+        return favoriteDate;
+    }
+
+    public void setFavoriteDate(Calendar favoriteDate) {
+        this.favoriteDate = favoriteDate;
+    }
+}
